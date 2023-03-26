ca198a593aa15cf1f1540ac4f5197ad0ad78af8b
jaason-shi <jasons8002@gmail.com>
2023-03-25 18:39:49 -0700

    create rectangle collision class

---
+++ src/main/java/ca/bcit/comp2522/termproject/penguinpursuit/Rectangle.java
@@ -0,0 +1,24 @@
+package ca.bcit.comp2522.termproject.penguinpursuit;
+
+public class Rectangle {
+    public double x;
+    public double y;
+    public double width;
+    public double height;
+
+    public Rectangle(double x, double y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+//  four cases; left of other rect, right of other rect, above other rect, below other rect
+    public boolean overlaps(Rectangle other) {
+        boolean noOverlap = this.x + this.width < other.x ||
+                other.x + other.width < this.x ||
+                this.y + this.height < other.y ||
+                other.y + other.height < this.y;
+        return !noOverlap;
+    }
+}

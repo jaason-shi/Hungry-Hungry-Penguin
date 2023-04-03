4ffe2722af9d10fc66c3b6df2eec75f21f24b478
nataliecly <nyu17@my.bcit.ca>
2023-04-02 21:43:31 -0700

    update Sprite class methods

--- src/main/java/ca/bcit/comp2522/termproject/penguinpursuit/Sprite.java
+++ src/main/java/ca/bcit/comp2522/termproject/penguinpursuit/Sprite.java
@@ -6,33 +6,30 @@ import javafx.scene.canvas.GraphicsContext;
 
 public class Sprite {
     public Vector position;
-    public Vector velocity;
+    public Vector speed;
     public Image image;
     public Rectangle boundary;
 
     public Sprite() {
         position = new Vector(0, 0);
-        velocity = new Vector(0, 0);
+        speed = new Vector(0, 0);
         boundary = new Rectangle(0, 0, 0, 0);
     }
 
-    public void setImage(String filename) {
-        image = new Image(filename);
+    public void setImage(String fileName) {
+        image = new Image(fileName);
         boundary.width = image.getWidth();
         boundary.height = image.getHeight();
     }
 
-    public void setPosition(double x, double y) {
-        position.set(x, y);
-
-    }
     public Rectangle getBoundary() {
         boundary.x = position.x;
         boundary.y = position.y;
         return boundary;
     }
-    public boolean overlaps(Sprite other) {
-        return getBoundary().overlaps(other.getBoundary());
+
+    public boolean intersects(Sprite other) {
+        return getBoundary().intersects(other.getBoundary());
     }
 
     public void render(GraphicsContext context) {

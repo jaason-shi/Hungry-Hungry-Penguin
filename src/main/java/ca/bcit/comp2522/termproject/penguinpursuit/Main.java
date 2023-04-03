7319e9916a49fcee4bdab0d3792fcdaf0aee1d33
nataliecly <nyu17@my.bcit.ca>
2023-04-02 21:56:24 -0700

    update user input methods

--- src/main/java/ca/bcit/comp2522/termproject/penguinpursuit/Main.java
+++ src/main/java/ca/bcit/comp2522/termproject/penguinpursuit/Main.java
@@ -1,12 +1,16 @@
 package ca.bcit.comp2522.termproject.penguinpursuit;
 
 import javafx.application.Application;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -24,110 +28,70 @@ public class Main extends Application {
 //        Parent root =  FXMLLoader.load(getClass().getResource("hello-view.fxml"));
 //        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
 
-        Group root = new Group();
-        Scene scene = new Scene(root, 800, 600, Color.SKYBLUE);
-        stage.getIcons().add(new Image("icon.png"));
+//        Group root = new Group();
+//        Scene scene = new Scene(root, 800, 600, Color.SKYBLUE);
+//        stage.getIcons().add(new Image("icon.png"));
         stage.setTitle("Penguin Pursuit");
-        stage.setResizable(false);
-        stage.setX(350);
-        stage.setY(100);
-
-        Text text = new Text();
-        text.setText("Penguin Pursuit");
-        text.setX(175);
-        text.setY(140);
-        text.setFont(Font.font("Verdana", FontWeight.BOLD, 50));
-        text.setFill(Color.NAVY);
-
-        Image image = new Image("icon.png");
-        ImageView imageView = new ImageView(image);
-        imageView.setX(290);
-        imageView.setY(220);
-
-        Group gameRoot = new Group();
-        Scene gameScene = new Scene(gameRoot, 800, 600, Color.SKYBLUE);
-
-        ArrayList<Platform> platformList = new ArrayList<Platform>();
-//        int platformCount = 10;
-//        for (int i=0; i<platformCount; i++){
-//            Platform platforms = new Platform();
-//            platforms.addPlatform(20, 20, 50, 50);
-//            platformList.add(platforms);
-//        }
-//        Platform platform = new Platform();
-        platformList.add(new Platform(0, 0, 50, 800));
-        platform.addPlatform(0, 0, 800, 50);
-        platform.addPlatform(0, 550, 800, 150);
-        platform.addPlatform(750, 0, 50, 800);
-
-        platform.addPlatform(110, 300, 50, 250);
-
-        Pane platformPane = new Pane();
-        for (Rectangle rectangle : platform.getPlatforms()) {
-            rectangle.setFill(Color.ALICEBLUE);
-            platformPane.getChildren().add(rectangle);
-        }
-
-        Platform icePlatform = new Platform();
-        icePlatform.addPlatform(0, 550, 300, 50);
-        Pane icePlatformPane = new Pane();
-        icePlatformPane.getChildren().add(icePlatform.getIcePlatform());
-
-////        Canvas canvas = new Canvas(600, 600);
-////        GraphicsContext context = canvas.getGraphicsContext2D();
-//        Sprite penguin = new Sprite();
-//        penguin.position.set(100, 100);
-//        penguin.setImage("penguin.png");
-////        penguin.render(context);
+//        stage.setResizable(false);
+//        stage.setScene(scene);
 //
+//        Text text = new Text();
+//        text.setText("Penguin Pursuit");
+//        text.setX(175);
+//        text.setY(140);
+//        text.setFont(Font.font("Verdana", FontWeight.BOLD, 50));
+//        text.setFill(Color.NAVY);
+//
+//        Image image = new Image("icon.png");
+//        ImageView imageView = new ImageView(image);
+//        imageView.setX(290);
+//        imageView.setY(220);
+
+        BorderPane gameRoot = new BorderPane();
+        Scene gameScene = new Scene(gameRoot);
+
+        stage.setScene(gameScene);
+        Canvas canvas = new Canvas(800, 600);
+        GraphicsContext context = canvas.getGraphicsContext2D();
+
+        gameRoot.setCenter(canvas);
+        context.setFill(Color.SKYBLUE);
+        context.fillRect(0,0,800,600);
+
 
         Player penguin = new Player(50, 50, Direction.RIGHT);
         Pane penguinPane = new Pane();
         penguinPane.getChildren().add(penguin.getPenguin());
 
-//    EventHandler<KeyEvent> keyListener = new EventHandler<KeyEvent>() {
-//        @Override
-//        public void handle(KeyEvent keyEvent) {
-//            if (keyEvent.getCode() == KeyCode.UP) {
-//                penguin.jump();
-//            }else if (keyEvent.getCode() == KeyCode.DOWN) {
-//                penguin.moveDown();
-//            } else if (keyEvent.getCode() == KeyCode.LEFT) {
-//                penguin.moveLeft();
-//            } else if (keyEvent.getCode() == KeyCode.RIGHT) {
-//                penguin.moveRight();
-//            }
-//            }
-//        };
 
+        ArrayList<Fish> fishList = new ArrayList<>();
+        for (int i = 1; i <= 15; i++) {
+            Fish fish = new Fish(25, 25);
+            fishList.add(fish);
 
-        private boolean isColliding(Player player, Platform platform){
-            Rectangle2D playerBounds = player.getBounds();
-            Rectangle2D platformBounds = platform.getBounds();
-            return playerBounds.intersects(platformBounds);
+            Pane fishPane = new Pane();
+            fishPane.getChildren().add(fish.getFish());
+            gameRoot.getChildren().add(fishPane);
         }
-//        check if penguin overlaps with platforms
-        gameScene.setOnKeyPressed(keyEvent -> {
-            switch(keyEvent.getCode()) {
-                case UP:
-                    penguin.moveUp(platformList);
-                    break;
-                case DOWN:
-                    penguin.moveDown(scene);
-                    break;
-                case LEFT:
-                    penguin.moveLeft();
-                    break;
-                case RIGHT:
-                    penguin.moveRight(scene);
-                    break;
-                default:
-                    break;
-            }
 
-            penguin.getPenguin().setX(penguin.getXCoordinate());
-            penguin.getPenguin().setY(penguin.getYCoordinate());
-        });
+
+        ArrayList<String> inputList = new ArrayList<>();
+
+        gameScene.setOnKeyPressed(
+                (KeyEvent event) -> {
+                    String keyName = event.getCode().toString();
+                    if (!inputList.contains(keyName)) {
+                        inputList.add(keyName);
+                    }
+                }
+        );
+
+        gameScene.setOnKeyReleased(
+                (KeyEvent event) -> {
+                    String keyName = event.getCode().toString();
+                    inputList.remove(keyName);
+                }
+        );
 
 
         Button button = new Button("Start");
